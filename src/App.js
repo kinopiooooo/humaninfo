4f96b3fb991712c5c7c37dd4138d6a4a1828791b
kinopiooooo <97212247+kinopiooooo@users.noreply.github.com>
2022-01-18 21:08:32 +0900

    good systaem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
-import { Badge, Container, ListGroup, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import React from 'react';
-import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import {Badge, Container, Figure, ListGroup, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
@@ -9,19 +8,21 @@ import axios from 'axios';
 
 function App() {
 
-  let [human, sethuman] = useState([{id: null, name: null, birth_y: null, birth_m: null, birth_d: null}]);
-  
+
+  let [human, setHuman] = useState([
+    {"id" : null,"name" : null,"birth_y" : null,"birth_m" : null,"birth_d" : null,"sex" : null, "photo" : null, 'good':null}
+  ]);
+
   useEffect(()=>{
-    axios.get('https://kinopiooooo.github.io/YoBatWedding/data.json')
+    axios.get('https://oiponik.github.io/YoBatWedding/data.json')
     .then((result)=>{
-      
-      sethuman([...result.data])
-     })
+      setHuman([...result.data])
+      console.log(human)
+      })
     .catch(()=>{ 
-      
       console.log('실패')
-     })
-   }, [])
+      })
+  }, [])
 
   return (
     <div className="App">
@@ -44,9 +45,37 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <List human={human}></List>
+      <HumanList human={ human } setHuman = {setHuman}></HumanList>
     </div>
   );
+}
+
+function HumanList(props){
+  return(
+    <ListGroup as="ol" numbered>
+    {
+       props.human.map((item, i)=>{
+         return(
+          <ListGroup.Item as="li" className="d-flex justify-content-between align-items-center" key={i}>
+            <div className='rounded overflow-hidden' style={{"width":"50px","height":"70px"}}>
+              <img src={props.human[i].photo} style={{"width":"100%","height":"100%","object-fit":"cover"}}/>
+            </div>
+            <div className="">
+              <div className="fw-bold">{props.human[i].name}</div>
+              <span>{props.human[i].birth_y}년 {props.human[i].birth_m}월 {props.human[i].birth_d}일</span>
+            </div>
+            <Badge variant="primary" pill onClick={()=>{
+              var array = [...props.human]
+              array[i].good = array[i].good + 1
+              props.setHuman(array)
+            }}>{props.human[i].good}</Badge>
+          </ListGroup.Item>
+        )
+      })
+    }
+      
+    </ListGroup>
+  )
 }
 
 
